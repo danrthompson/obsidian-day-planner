0399722db38cdfaff5654d8cbf4818b54afa619d
Dan Thompson <danielrthompsonjr@gmail.com>
2022-09-17 16:56:19 -0400

    fix time sort

--- src/plan-data.ts
+++ src/plan-data.ts
@@ -1,4 +1,4 @@
-import type { DayPlannerSettings } from './settings';
+import type { DayPlannerSettings } from "./settings";
 
 const moment = (window as any).moment;
 
@@ -9,48 +9,51 @@ export class PlanSummaryData {
     past: PlanItem[];
     current: PlanItem;
     next: PlanItem;
-    
-    constructor(items: PlanItem[]){
+
+    constructor(items: PlanItem[]) {
         this.empty = items.length < 1;
         this.invalid = false;
-        this.items = items;
+        this.items = items.sort((a, b) => (a.time < b.time ? -1 : 1));
         this.past = [];
     }
 
     calculate(): void {
         try {
             const now = new Date();
-            if(this.items.length === 0){
+            if (this.items.length === 0) {
                 this.empty = true;
                 return;
             }
             this.items.forEach((item, i) => {
-                const next = this.items[i+1];
-                if(item.time < now && (item.isEnd || (next && now < next.time))) {
+                const next = this.items[i + 1];
+                if (
+                    item.time < now &&
+                    (item.isEnd || (next && now < next.time))
+                ) {
                     this.current = item;
                     if (item.isEnd) {
                         item.isPast = true;
                         this.past.push(item);
                     }
                     this.next = item.isEnd ? null : next;
-                } else if(item.time < now) {
+                } else if (item.time < now) {
                     item.isPast = true;
                     this.past.push(item);
                 }
-                if(next){
-                    const untilNext = moment.duration(moment(next.time).diff(moment(item.time))).asMinutes();
+                if (next) {
+                    const untilNext = moment
+                        .duration(moment(next.time).diff(moment(item.time)))
+                        .asMinutes();
                     item.durationMins = untilNext;
                 }
             });
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
-
 }
 
 export class PlanItem {
-
     matchIndex: number;
     charIndex: number;
     isCompleted: boolean;
@@ -63,8 +66,17 @@ export class PlanItem {
     text: string;
     raw: string;
 
-    constructor(matchIndex: number, charIndex: number, isCompleted: boolean, 
-        isBreak: boolean, isEnd: boolean, time: Date, rawTime:string, text: string, raw: string){
+    constructor(
+        matchIndex: number,
+        charIndex: number,
+        isCompleted: boolean,
+        isBreak: boolean,
+        isEnd: boolean,
+        time: Date,
+        rawTime: string,
+        text: string,
+        raw: string
+    ) {
         this.matchIndex = matchIndex;
         this.charIndex = charIndex;
         this.isCompleted = isCompleted;
@@ -84,16 +96,36 @@ export class PlanItemFactory {
         this.settings = settings;
     }
 
-    getPlanItem(matchIndex: number, charIndex: number, isCompleted: boolean, isBreak: boolean, isEnd: boolean, time: Date, rawTime: string, text: string, raw: string) {
+    getPlanItem(
+        matchIndex: number,
+        charIndex: number,
+        isCompleted: boolean,
+        isBreak: boolean,
+        isEnd: boolean,
+        time: Date,
+        rawTime: string,
+        text: string,
+        raw: string
+    ) {
         const displayText = this.getDisplayText(isBreak, isEnd, text);
-        return new PlanItem(matchIndex, charIndex, isCompleted, isBreak, isEnd, time, rawTime, displayText, raw);
+        return new PlanItem(
+            matchIndex,
+            charIndex,
+            isCompleted,
+            isBreak,
+            isEnd,
+            time,
+            rawTime,
+            displayText,
+            raw
+        );
     }
 
     getDisplayText(isBreak: boolean, isEnd: boolean, text: string) {
-        if(isBreak) {
+        if (isBreak) {
             return this.settings.breakLabel;
         }
-        if(isEnd) {
+        if (isEnd) {
             return this.settings.endLabel;
         }
         return text;
